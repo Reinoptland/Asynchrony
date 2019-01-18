d1af318ecf66067ae74600c90d594afa7fc1bf2d
reinoptland <reinoptland@gmail.com>
2019-01-18 13:58:43 +0100

    calling getDocument

---
+++ documents-authors.js
@@ -0,0 +1,28 @@
+const documents = {
+    1: {content: "I am a document", authorId: 1},
+    2: {content: "I am also a document", authorId: 2},
+    3: {content: "You guessed it!", authorId: 2}
+}
+const authors = {
+    1: {name: "Adam"},
+    2: {name: "Dave"}
+}
+
+function getDocument(documentId, callback) {
+    function loadComplete() {
+        callback(documents[documentId])
+    }
+    setTimeout(loadComplete, 2000)
+}
+
+function getAuthor(authorId, callback) {
+    function loadComplete() {
+        callback(authors[authorId])
+    }
+    setTimeout(loadComplete, 100)
+}
+
+getDocument(2, (document) => {
+    console.log(document.authorId)
+    // use getAuthor with authorId to 
+})
