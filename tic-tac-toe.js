53c827b88241e3bae8523137c1e751abe094854c
reinoptland <reinoptland@gmail.com>
2019-01-20 12:24:57 +0100

    An even more elegant calculateWinner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,49 +1,31 @@
 const board = [
-    [null, 'x', 'x'],
-    ['o', 'o', null],
-    ['x', 'x', 'x']
+    ['x', 'x', 'o'],
+    ['x', 'o', null],
+    ['o', 'x', 'x']
 ]
 
 const isWinningLine = line => line[0] !== null && new Set(line).size === 1
 
 const calculateWinner = (board) => {
-    const rows = board.map(row => [...row])
-    const { columns, diagonals } = transposeToColumnsAndDiagonals(board)
-    const lines = [ ...rows, ...columns, ...diagonals]
-    const winningLine = lines.find(isWinningLine)
+    const winningLine = board.find((row, rowIndex, board) => {
+        if(isWinningLine(row)) return row
+
+        const column = board.map(row => row[rowIndex])
+        if(isWinningLine(column)) return column
+
+        // If we've checked all rows & columns and we have no winner yet,
+        if(rowIndex === board.length - 1){
+            // we check both diagonals
+            const diagonal1 = board.map((row, index) => row[index])
+            if(isWinningLine(diagonal1)) return diagonal1
+
+            const diagonal2 = board.map((row, index) => row[board.length - 1 - index])
+            if(isWinningLine(diagonal2)) return diagonal2
+        }
+    })
 
     if(winningLine) return winningLine[0]
     return null
-}
-
-
-function transposeToColumnsAndDiagonals(board) {
-    return board.reduce((boardAcc, row, rowIndex, board) => {
-        // Since we're going over all symbols we might as well do columns, diagonals at once
-        let { columns, diagonals } = row.reduce((rowAcc, symbol, index) => {
-            // convert rows to columns
-            const columns = [ ...rowAcc.columns ]
-            columns[index] = columns[index].concat(symbol)
-
-            // Check if the current symbol is part of either diagonal
-            // If so, add them to a diagonal array
-            let diagonals = [...rowAcc.diagonals]
-            if (rowIndex === index) {
-                diagonals[0] = diagonals[0].concat(symbol)
-            }
-
-            if (index === (board.length - 1) - rowIndex) {
-                diagonals[1] = diagonals[1].concat(symbol)
-            }
-
-            return { columns, diagonals};
-        }, { columns: [...boardAcc.columns], diagonals: [...boardAcc.diagonals]});
-
-        return {
-            columns,
-            diagonals
-        };
-    }, { columns: new Array(board.length).fill([]), diagonals: [[], []] });
 }
 
 const displayWinner = () => {
